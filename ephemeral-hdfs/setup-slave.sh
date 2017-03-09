078a27ec7d61995dd29e3e49f1e357536509e7c0
frankfzw <frankfzw@gmail.com>
2017-03-09 10:28:47 +0800

    chown of hdfs dir

--- ephemeral-hdfs/setup-slave.sh
+++ ephemeral-hdfs/setup-slave.sh
@@ -2,11 +2,15 @@
 
 # Setup ephemeral-hdfs
 mkdir -p /mnt/ephemeral-hdfs/logs
+chown ubuntu:ubuntu /mnt/ephemeral-hdfs/logs
 mkdir -p /mnt/hadoop-logs
+chown ubuntu:ubuntu /mnt/hadoop-logs
 
 # Setup yarn logs, local dirs
 mkdir -p /mnt/yarn-local
+chown ubuntu:ubuntu /mnt/yarn-local
 mkdir -p /mnt/yarn-logs
+chown ubuntu:uubntu /mnt/yarn-logs
 
 # Create Hadoop and HDFS directories in a given parent directory
 # (for example /mnt, /mnt2, and so on)
@@ -14,8 +18,12 @@ function create_hadoop_dirs {
   location=$1
   if [[ -e $location ]]; then
     mkdir -p $location/ephemeral-hdfs $location/hadoop/tmp
+    chown ubuntu:ubuntu $location/ephemeral-hdfs
+    chown ubuntu:ubuntu $location/hadoop/tmp
     chmod -R 755 $location/ephemeral-hdfs
     mkdir -p $location/hadoop/mrlocal $location/hadoop/mrlocal2
+    chown ubuntu:ubuntu $location/hadoop/mrlocal
+    chown ubuntu:ubuntu $location/hadoop/mrlocal2
   fi
 }
 
