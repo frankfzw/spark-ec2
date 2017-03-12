fd278d2fe90e65b67ea3739cce689f9953c628fc
frankfzw <frankfzw@gmail.com>
2017-03-12 15:25:17 +0800

    add optin to start spark cluster

--- start-stop.py
+++ start-stop.py
@@ -3,23 +3,35 @@
 import sys
 import os
 
-usg = "Usage ./start-stop.py start/stop cluster_name"
+usg = "Usage ./start-stop.py start/stop cluster_type(spark/hadoop) cluster_name"
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
     print usg
     sys.exit()
 
 action = sys.argv[1]
-name = sys.argv[2]
+cluster_type = sys.argv[2]
+name = sys.argv[3]
 
 cmd = ''
 
-if action == 'start':
-    cmd = "./spark-ec2 -k scache -i ~/SCache/scache.pem -r us-west-2 --zone=us-west-2a -u ubuntu --hadoop-major-version=yarn --no-ganglia start %s" % name
-    os.system(cmd)
-elif action == 'stop':
-    cmd = "./spark-ec2 -k scache -i ~/SCache/scache.pem -r us-west-2 --zone=us-west-2a -u ubuntu stop %s" % name
-    os.system(cmd)
+if cluster_type == 'spark':
+    if action == 'start':
+        cmd = "./spark-ec2 -k scache -i ~/SCache/scache.pem -r us-west-2 --zone=us-west-2a --hadoop-major-version=yarn --no-ganglia start %s" % name
+        os.system(cmd)
+    elif action == 'stop':
+        cmd = "./spark-ec2 -k scache -i ~/SCache/scache.pem -r us-west-2 --zone=us-west-2a stop %s" % name
+        os.system(cmd)
+    else:
+        print usg
+elif cluster_type == 'hadoop':
+    if action == 'start':
+        cmd = "./spark-ec2 -k scache -i ~/SCache/scache.pem -r us-west-2 --zone=us-west-2a -u ubuntu --hadoop-major-version=yarn --no-ganglia start %s" % name
+        os.system(cmd)
+    elif action == 'stop':
+        cmd = "./spark-ec2 -k scache -i ~/SCache/scache.pem -r us-west-2 --zone=us-west-2a -u ubuntu stop %s" % name
+        os.system(cmd)
+    else:
+        print usg
 else:
     print usg
-
